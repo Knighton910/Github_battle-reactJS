b5799d2c311b941fe3e3bb1275ddb503d54ff755
kelvin knigton <keltheceo@gmail.com>
2016-05-26 22:59:00 -0700

    updated index: working

--- app/index.js
+++ app/index.js
@@ -1,36 +1,32 @@
 var UD = {
-  name: 'Tlyer McGinnis',
-  username: 'tylermcginnis',
-  image: 'https://avatars0.githubusercontent.com/u/2933430?v=3&s=460'
+  name: 'Kelvin Kni',
+  username: 'lordKnighton',
+  image: 'https://avatars1.githubusercontent.com/u/1773260?v=3&s=460'
 }
 
-var React = require('react')
-var ReactDOM = require('react-dom')
+var React = require('react');
+var ReactDOM = require('react-dom');
 
 
-
-// profilepic component
-var ProfilePic = react.createClass({
+ProfilePic = React.createClass({
   render: function() {
     return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
   }
 })
 
-// profilelink component
-var ProfileLink = react.createClass({
+ProfileLink = React.createClass({
   render: function() {
     return (
       <div>
-        <a href = {'https://www.github.com/ '+ this.props.username}>
-        {this.props.username}
+        <a href={'https://www.github.com/' +this.props.username}>
+          {this.props.username}
         </a>
       </div>
     )
   }
 })
 
-// profilename component
-var ProfileName = react.createClass({
+ProfileName = React.createClass({
   render: function() {
     return (
       <div>{this.props.name}</div>
@@ -38,19 +34,17 @@ var ProfileName = react.createClass({
   }
 })
 
-// Avatar component
-var Avatar = react.createClass({
+Avatar = React.createClass({
   render: function() {
     return (
       <div>
         <ProfilePic imageUrl={this.props.user.image} />
         <ProfileName name={this.props.user.name} />
-        <ProfileLink username={this.props.user.username}/>
+        <ProfileLink username={this.props.user.username} />
       </div>
     )
   }
 })
-
 
 ReactDOM.render(
   <Avatar user={UD} />,
